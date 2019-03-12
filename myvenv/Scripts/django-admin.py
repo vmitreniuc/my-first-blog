7691b97244f3b9da5d23084825dc4781cc22064c
Victor <vmitreniuc@gmail.com>
2019-03-12 08:43:59 +0200

    Моя первая резервная копия

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\viteok_documente\proiecte viteok\web proiecte\django\djangogirls\myvenv\scripts\python37.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
